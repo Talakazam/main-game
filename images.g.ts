8bced50e56c14f64871451efea0d543f1599fd66
Talakazam <40490205+Talakazam@users.noreply.github.com>
2023-10-26 16:05:10 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,64 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "blue circle":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 1 . . . . 
+. . . . 1 f f f f f f f 1 . . . 
+. . . 1 f 9 9 9 9 9 9 9 f 1 . . 
+. . 1 f 9 9 9 9 9 9 9 9 9 f 1 . 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. 1 f 9 9 9 9 9 9 9 9 9 9 9 f 1 
+. . 1 f 9 9 9 9 9 9 9 9 9 f 1 . 
+. . . 1 f 9 9 9 9 9 9 9 f 1 . . 
+. . . . 1 f f f f f f f 1 . . . 
+. . . . . 1 1 1 1 1 1 1 . . . . 
+`;
+            case "image2":
+            case "red circle0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 1 . . . . 
+. . . . 1 f f f f f f f 1 . . . 
+. . . 1 f 2 2 2 2 2 2 2 f 1 . . 
+. . 1 f 2 2 2 2 2 2 2 2 2 f 1 . 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. 1 f 2 2 2 2 2 2 2 2 2 2 2 f 1 
+. . 1 f 2 2 2 2 2 2 2 2 2 f 1 . 
+. . . 1 f 2 2 2 2 2 2 2 f 1 . . 
+. . . . 1 f f f f f f f 1 . . . 
+. . . . . 1 1 1 1 1 1 1 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "ZELDA2":return hex`004b000408140108001c000e050046006603320000040a002d00000064001400010a0002000002240004000800011e08000c0001220c001000012414001800011e18001c0001221c0038000124`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
